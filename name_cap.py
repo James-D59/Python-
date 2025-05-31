646e602f1447798148719e33a77c9d194c0d9132
James <james_dittmar@yahoo.com>
2025-05-31 16:20:22 -0500

    capitalizes names entered

---
+++ name_cap.py
@@ -0,0 +1,10 @@
+#input a name and capitalize it
+
+#I always like to clear screen after use 
+import os
+os.system("clear")
+
+
+name = input("Enter your name wihtout capitalizing anything: ")
+
+print(f'Watch me add in the capitizing! {name.title()} ')
