5068a5659e5bfc6bdc528a6b376979104a80e29a
James <james_dittmar@yahoo.com>
2025-11-02 18:28:03 -0600

    Add files via upload

--- todo.py
+++ todo.py
@@ -21,18 +21,38 @@ def add_task(tasks, task):
 
 
 # View all tasks in the list
-def view_tasks():
-    pass
+def view_tasks(tasks):
+    if not tasks:
+        print("No tasks in the list.")
+    else:
+        print("\n--- To-Do List ---")
+        for idx, task_info in enumerate(tasks, start=1):
+            status = "Done" if task_info["completed"] else "Not Done"
+            print(f"{idx}. {task_info['task']} - {status}")
+    print()
+
+
+            
 
 # Remove a task from the list
-def remove_task():
-    pass
+def remove_task(tasks, task_index):
+    if 0 <= task_index < len(tasks):
+        removed_task = tasks.pop(task_index)
+        print(f'Task "{removed_task["task"]}" has been removed.')
+    else:
+        print("Invalid task number.")
+    return tasks
 
 # Mark task as complete
-def mark_task_complete():
-    pass
+def mark_task_complete(tasks, task_index):
+    if 0 <= task_index < len(tasks):
+        tasks[task_index]["completed"] = True
+        print(f'Task "{tasks[task_index]["task"]}" has been marked as complete.')
+    else:
+        print("Invalid task number.")
+    return tasks
 
-# Main todo function
+# Main to do function
 def to_do_list_app():
     # Create a python list to keep track of our to do list
     tasks = []
@@ -54,15 +74,36 @@ def to_do_list_app():
             #tell the user to hit enter
             input("Press Enter to Continue...") #puase before clearing the screen
         elif choice == '2':
-            view_tasks()
+            view_tasks(tasks)
+            input("Press Enter to Continue...")
+        
         elif choice == '3':
-            remove_task()
+            if tasks:
+                view_tasks(tasks) #show the list before prompting the user to remove one
+                task_index = int(input("Enter the task number to remove: ")) - 1
+                # call the remove task function and pass intm to remove he list
+                tasks = remove_task(tasks, task_index)
+            else:
+                print("There are no tasks to remove...")
+            input("Press Enter to Continue...")
+            
         elif choice == '4':
-            mark_task_complete()
+            if tasks:
+                view_tasks(tasks) #show the list before prompting the user to mark one complete
+                task_index = int(input("Enter the task number to mark as complete: ")) - 1
+                tasks = mark_task_complete(tasks, task_index)
+            else:
+                print("There are no tasks to mark as complete...")
+
+            input("Press Enter to Continue...")
+        
         elif choice == '5':
+            print("Exiting the To-Do List. Goodbye!")
             break
+
         else:
             print("Invalid choice. Please try again.")
+            input("Press Enter to Continue...")
 
 # Run the to do list application
 to_do_list_app()
